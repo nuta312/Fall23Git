3948fa98246009350b370a56560b64c033326697
Nursultan <musakunovnur@gmail.com>
2024-01-25 09:40:15 +0600

    C1-commit-3

--- src/C/C1.java
+++ src/C/C1.java
@@ -4,4 +4,8 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
 }
